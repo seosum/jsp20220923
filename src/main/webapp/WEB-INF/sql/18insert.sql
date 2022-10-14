6329f3c7890abd0b8b666c3b91cc6e0983fbc64f
seosum <ssm347574248@gmail.com>
2022-10-14 16:57:04 +0900

    20221014

--- src/main/webapp/WEB-INF/sql/18insert.sql
+++ src/main/webapp/WEB-INF/sql/18insert.sql
@@ -1,6 +1,6 @@
 USE w3schools;
 
-SELECT * FROM Employees;
+SELECT * FROM Employees ORDER BY EmployeeID DESC;
 -- INSERT INTO : 테이블에 레코드 추가하는 명령문
 INSERT INTO Employees 
 (EmployeeID, LastName, FirstName, BirthDate, Photo, Notes)
@@ -20,5 +20,41 @@ VALUES
 (12, 'Black', 'Panther', '1970-01-01', 'EmpID12.pic');
 
 -- 13번째 직원 추가, birthDate, notes 컬럼 빼고
--- NULL은  값이 없음 
+INSERT INTO Employees
+(EmployeeID, LastName, FirstName, Photo)
+VALUES
+(13, 'Doctor', 'Strange', 'EmpID13.pic');
 
+-- 새 직원 추가 , EmployeeID컬럼 빼고
+INSERT INTO Employees
+(LastName, FirstName)
+VALUES
+('Natasha', 'Romanoff');
+
+INSERT INTO Employees
+(LastName, FirstName, Notes)
+VALUES
+('Kim', 'kildong', '');
+
+INSERT INTO Employees
+(LastName, FirstName, Notes)
+VALUES
+('Kim', 'kildong2', ' ');
+
+-- NULL은 값이 없음
+
+-- Customers 테이블에 레코드 추가
+SELECT * FROM Customers ORDER BY 1 DESC;
+
+INSERT INTO Customers
+(CustomerID, CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES
+(100, 'Steve', 'Captain', 'brooklyn', 'NewYork', '33333', 'USA');
+
+INSERT INTO Customers
+(CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES
+('Stephen', 'Strange', 'brooklyn', 'NewYork', '33333', 'USA');
+
+-- DESCRIBE : 테이블 구조 조회, 제약사항(Constraints) 조회
+DESC Customers; 
